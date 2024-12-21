014f6f62945adf512a74950c0971d8d6ddae81b3
Ali Sanad <alisanad.dev@gmail.com>
2024-12-21 21:09:48 +0200

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/todo-list/',
   build: {
     cssMinify: 'esbuild',
     minify: 'esbuild'
